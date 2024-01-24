5ffa2aff7cf13df31354371ada88bb6af1570e44
M. M. Adeel <54585222+Adeel3Dgenomics@users.noreply.github.com>
2024-01-24 14:22:48 -0500

    Update and rename post_TE.sh to post_TE.R

--- post_TE.R
+++ post_TE.R
@@ -1,14 +1,11 @@
+
 clean <- read.delim("ab24_count_TE.txt")
-head(clean)
 reference_table <- read.delim("reference_table.txt")
 common_values <- merge(reference_table, clean, by = 'Name', all = TRUE) ##add NAs in mismatched
 common_values1 <- merge(clean, reference_table, by = 'Name', all = TRUE)
-view(common_values1)
-view(common_values)
 common_values$Morc3ab_mut_Ave<-(c(common_values$morc3b.24hpf_rep1.T+common_values$morc3b.24hpf_rep2.T+common_values$morc3b.24hpf_rep3.T)/3)
 common_values$Morc3ab_wt_Ave<-(c(common_values$wt_24hpf_rep1.C+common_values$wt_24hpf_rep2.C+common_values$wt_24hpf_rep3.C)/3)
 write.table(common_values, file = "morc3b_24hpf_final.txt", sep = "\t", quote = F, row.names = T)
 
 clean_final<-na.omit(common_values)
-View(clean_final)
 write.table(clean_final, "morcb24_final_TE.txt", sep = "\t", quote=F)
