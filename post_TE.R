e946dab40f6fcb7230d7d0e2507b894135308a88
M. M. Adeel <54585222+Adeel3Dgenomics@users.noreply.github.com>
2024-01-24 14:27:37 -0500

    Update post_TE.R

--- post_TE.R
+++ post_TE.R
@@ -6,6 +6,5 @@ common_values1 <- merge(clean, reference_table, by = 'Name', all = TRUE)
 common_values$Morc3ab_mut_Ave<-(c(common_values$morc3b.24hpf_rep1.T+common_values$morc3b.24hpf_rep2.T+common_values$morc3b.24hpf_rep3.T)/3)
 common_values$Morc3ab_wt_Ave<-(c(common_values$wt_24hpf_rep1.C+common_values$wt_24hpf_rep2.C+common_values$wt_24hpf_rep3.C)/3)
 write.table(common_values, file = "morc3b_24hpf_final.txt", sep = "\t", quote = F, row.names = T)
-
 clean_final<-na.omit(common_values)
 write.table(clean_final, "morcb24_final_TE.txt", sep = "\t", quote=F)
