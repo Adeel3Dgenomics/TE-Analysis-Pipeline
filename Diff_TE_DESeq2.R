1325cd505ba1ab3084e29e37ccc4b52f136e224e
M. M. Adeel <54585222+Adeel3Dgenomics@users.noreply.github.com>
2024-01-30 16:08:15 -0500

    Update Diff_TE_DESeq2.R

--- Diff_TE_DESeq2.R
+++ Diff_TE_DESeq2.R
@@ -1,13 +1,14 @@
-data <- read.table("test_TE_only.cnTable",header=T,row.names=1)
+data <- read.table("TEs_only_count.cnTable",header=T,row.names=1)
 groups <- factor(c(rep("TGroup",3),rep("CGroup",3)))
-min_read <- 0
+min_read <- 10 ##consider only the TEs higher then 10 read counts
 data <- data[apply(data,1,function(x){max(x)}) > min_read,]
+write.table(data,"clean_TE_count.txt", sep = "\t", quote = F)
 sampleInfo <- data.frame(groups,row.names=colnames(data))
 suppressPackageStartupMessages(library(DESeq2))
 dds <- DESeqDataSetFromMatrix(countData = data, colData = sampleInfo, design = ~ groups)
 dds$groups = relevel(dds$groups,ref="CGroup")
 dds <- DESeq(dds)
 res <- results(dds)
-write.table(res, file="1_new_25hpf_control_gene_TE.txt", sep="\t",quote=F)
-resSig <- res[(!is.na(res$padj) & (res$padj < 0.050000) &         (abs(res$log2FoldChange)> 2.000000)), ]
-write.table(resSig, file="new_25hpf_control_sigdiff_gene_TE_Only.txt",sep="\t", quote=F)
+write.table(res, file="expressed_TE.txt", sep="\t",quote=F)
+##resSig <- res[(!is.na(res$padj) & (res$padj < 0.050000) & (abs(res$log2FoldChange)> 2.000000)), ] 
+##write.table(resSig, file="new_25hpf_control_sigdiff_gene_TE_Only.txt",sep="\t", quote=F)
